57f36cfc9cfb647cf2437eeb4396c30d479d2aea
SlavaShedu <shedu.code@gmail.com>
2020-12-29 22:03:20 +0300

    added some new tests

--- Task_1_Anagrams/test/com/cleancode/anagrams/AnagramsTest.java
+++ Task_1_Anagrams/test/com/cleancode/anagrams/AnagramsTest.java
@@ -21,9 +21,13 @@ class AnagramsTest {
 	}
 
 	public static Stream<Arguments> argumentsProvider() {
-		return Stream.of(Arguments.arguments("a1bcd efg!h", "d1cba hgf!e"), Arguments.arguments("abcd", "dcba"),
+		return Stream.of(Arguments.arguments("a1bcd efg!h", "d1cba hgf!e"),
+				Arguments.arguments("abc2f x34 !!@ 1fgr7", "fcb2a x34 !!@ 1rgf7"), Arguments.arguments("abcd", "dcba"),
 				Arguments.arguments("&%$!5-*@", "&%$!5-*@"), Arguments.arguments("a", "a"),
-				Arguments.arguments("!", "!"), Arguments.arguments("", ""), Arguments.arguments(" ", ""));
+				Arguments.arguments("aaaaa", "aaaaa"), Arguments.arguments("aaaaAAaaAaA", "AaAaaAAaaaa"),
+				Arguments.arguments("ABC", "CBA"), Arguments.arguments("abc", "cba"), Arguments.arguments("!", "!"),
+				Arguments.arguments("", ""), Arguments.arguments(" ", ""),
+				Arguments.arguments("  ", ""));
 	}
 
 	@Test
