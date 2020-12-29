88e4036532c526547dbab34807399e43937cae40
SlavaShedu <shedu.code@gmail.com>
2020-12-29 19:40:13 +0300

    corrected test names

--- Task_1_Anagrams/test/com/cleancode/anagrams/AnagramsTest.java
+++ Task_1_Anagrams/test/com/cleancode/anagrams/AnagramsTest.java
@@ -16,56 +16,56 @@ class AnagramsTest {
 	}
 
 	@Test
-	public void testBuildAnagram_SeveralWords_ShouldReverseSeparatelly() {
+	public void buildAnagram_shouldReverseSeparatelly_whenSeveralWords() {
 		String actual = "a1bcd efg!h";
 		String expected = "d1cba hgf!e";
 		assertEquals(expected, anagram.buildAnagram(actual));
 	}
 
 	@Test
-	public void testBuildAnagram_EmptyString_ShouldReturnEmptyString() {
+	public void buildAnagram_shouldReturnEmptyString_whenEmptyString() {
 		String actual = "";
 		String expected = "";
 		assertEquals(expected, anagram.buildAnagram(actual));
 	}
 
 	@Test
-	public void testBuildAnagram_Space_ShouldReturnEmptyString() {
+	public void buildAnagram_shouldReturnEmptyString_whenSpace() {
 		String actual = " ";
 		String expected = "";
 		assertEquals(expected, anagram.buildAnagram(actual));
 	}
 
 	@Test
-	public void testBuildAnagram_OnlyCharacters_ShouldReverse() {
+	public void buildAnagram_shouldReverse_whenOnlyCharacters() {
 		String actual = "abcd";
 		String expected = "dcba";
 		assertEquals(expected, anagram.buildAnagram(actual));
 	}
 
 	@Test
-	public void testBuildAnagram_OnlyNonCharacters_ShoulNotReverse() {
+	public void buildAnagram_shoulNotReverse_whenOnlyNonCharacters() {
 		String actual = "&%$!5-*@";
 		String expected = "&%$!5-*@";
 		assertEquals(expected, anagram.buildAnagram(actual));
 	}
 
 	@Test
-	public void testBuildAnagram_SingleLetter_ShouldNotDoAnithing() {
+	public void buildAnagram_shouldNotDoAnithing_whenSingleLetter_() {
 		String actual = "a";
 		String expected = "a";
 		assertEquals(expected, anagram.buildAnagram(actual));
 	}
 
 	@Test
-	public void testBuildAnagram_SingleNonLetter_ShouldNotDoAnithing() {
+	public void buildAnagram_shouldNotDoAnithing_whenSingleNonLetter() {
 		String actual = "!";
 		String expected = "!";
 		assertEquals(expected, anagram.buildAnagram(actual));
 	}
 
 	@Test
-	public void testBuildAnagram_NullInput_ShouldThrow() {
+	public void buildAnagram_ShouldThrow_when_bullInput() {
 		assertThrows(NullPointerException.class, () -> anagram.buildAnagram(null));
 	}
 
